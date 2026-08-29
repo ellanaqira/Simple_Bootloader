e7fe94f4eedbc04204b174c1bbf18c99fd291eb5
ellanaqira <ellanrepo@gmail.com>
2026-08-29 23:38:32 +0800

    feat: create a simple boot program that display anagram to practice addressing

--- exercise.asm
+++ exercise.asm
@@ -2,16 +2,35 @@
 
 mov ah, 0x0e
 
+mov bx, str1 + 2
+mov al, [bx]
+int 0x10
+
+mov bx, str1 + 1
+mov al, [bx]
+int 0x10
+
 mov bx, str1
 mov al, [bx]
 int 0x10
 
+mov bx, str1 + 4
+mov al, [bx]
+int 0x10
+
+mov bx, str1 + 5
+mov al, [bx]
+int 0x10
+
+mov bx, str1 + 3
+mov al, [bx]
+int 0x10
 
 jmp $
 
 
 str1:
-    db "Xo"
+    db "listen"
 
 times 510-($-$$) db 0
 
